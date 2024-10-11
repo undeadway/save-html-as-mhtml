a7a38dfe0803f4978dcd2dac4e5868dad7466b54
weigucheng <weigucheng@i-hc.cn>
2024-10-11 14:43:46 +0800

    删除不要的定义

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -9,14 +9,11 @@ const QUOTED_PRINTABLE = "quoted-printable";
 const AT_MHTML_BLINK = "@mhtml.blink";
 
 const WINDOWS_PATH_REGX = /^[a-zA-Z]:/;
-const HTML_IMAGE_REGX = /<img id="#p(\d)+" src="(\S{1,})" \/>/;
-const EXTRACTS_COLOR_REGX = /#\[([0-9a-fA-F]{6})\]\{([\S\s]+?)\}/;
-const EXTRACTS_AT_ITEMS_REGX = /@\[(image|icon|table|anchor)\]\{(\S+?)\}/;
+const HTML_IMAGE_REGX = /<img src="(\S{1,})" \/>/;
 
 const HTTP = "http", HTTPS = "https";
-const MK_POINT = ".", MK_DASH = "-", MK_SLASH = "/", MK_COLON = ":";
-const BLANK = "", SPACE = " ";
-const HTML_BR = "<br />";
+const MK_POINT = ".", MK_DASH = "-", MK_SLASH = "/";
+const BLANK = "";
 
 
 module.exports = exports = {
@@ -27,8 +24,6 @@ module.exports = exports = {
     DIGIT,
     WINDOWS_PATH_REGX,
     HTML_IMAGE_REGX,
-    EXTRACTS_COLOR_REGX,
-    EXTRACTS_AT_ITEMS_REGX,
     HTTP,
     HTTPS,
     MIME_TEXT_CSS,
@@ -38,8 +33,5 @@ module.exports = exports = {
     MK_DASH,
     MK_POINT,
     MK_SLASH,
-    MK_COLON,
     BLANK,
-    SPACE,
-    HTML_BR
 };
